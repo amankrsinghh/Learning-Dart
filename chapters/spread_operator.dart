8bc7b56c8a89f9ba914542c20fabca66b7c1078e
Aman Singh <xaman282@gmail.com>
2026-06-09 01:57:16 +0530

    fix: correct formatting and improve readability in spread operator example

--- chapters/spread_operator.dart
+++ chapters/spread_operator.dart
@@ -1,7 +1,17 @@
 void main() {
   List<String> name = ["aman", "rahul", "satya", "padosi"];
   List<String> city = ["patna", "gaya", "chapra", "danapur"];
-  List<String>? product ;
-  List<String> nameandcity = [...name, ...city,...?product];
+  List<String>? product;
+  List<String> nameandcity = [...name, ...city, ...?product];
   print(nameandcity);
+
+  //for each loop in dart
+  for (String item in nameandcity) {
+    print(item);
+  }
+  // we can alsp perform for loop or conditional statement in List
+  //example
+  List<int> n1 = [1, 2, 3];
+  List<int> n2 = [4, 5, 6, for (int a in n1) a * 2, ...n1];
+  print(n2);
 }
